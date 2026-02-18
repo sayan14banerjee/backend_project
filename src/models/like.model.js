79efb6fc30cf46a4c402c3676f9ad45358bc8065
sayan14banerjee <banerjeesayan554@gmail.com>
2026-02-18 21:37:34 +0530

    comment, tweet, like, playlist controllers

---
+++ src/models/like.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema({
+    video: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+        required: true,
+    },
+    tweet: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tweet",
+        required: true,
+    },
+    comment: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+        required: true,
+    },
+    likedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+},
+{    timestamps: true,
+});
+
+export const Like = mongoose.model("Like", likeSchema);
---
+++ src/models/playlist.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const playlistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    videos: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+    }],
+    description: {
+        type: String,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+},
+{    timestamps: true,});
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
